b5affb02ea1cec78b6cef1c3ec591bf8afd8b541
TimCTaylor <editors@greyhartpress.com>
2026-03-20 17:53:52 +0000

    implemented venue and add on selection

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -3,10 +3,13 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
+
 const ConferenceEvent = () => {
   const [showItems, setShowItems] = useState(false);
   const [numberOfPeople, setNumberOfPeople] = useState(1);
   const venueItems = useSelector((state) => state.venue);
+  const avItems = useSelector((state) => state.av);
   const dispatch = useDispatch();
   const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -23,15 +26,39 @@ const ConferenceEvent = () => {
     dispatch(incrementQuantity(index));
   };
 
+  {/* These dispatches seem the most React-Redux part of the code, so let's see if I have this right.
+      Rather than handle the state changes directly in the component, I'm using Redux store to manage state.
+      It's more code and complexity than makes sense for this little app if I were doing it for real, rather than as an exercise.
+      But as we scale up into larger apps, it makes more sense as it means we manage state in a central place and can share it across components more easliy.
+      I can also see how it would lead to nicer separation of concerns, to keep state management separate from the UI code.  
+      
+      Anyway, back to the code:
+      These handler functions are linked to the onClick events of the buttons in the return statement for the ConferenceEvent component. 
+      When a button is clicked, the corresponding handler function is called with the index of the item being modified.
+
+      dispatch is a function provided by the useDispatch hook from React-Redux. It allows us to send actions to the Redux store to update the state.
+
+      decrementQuantity is an action creator imported from the venueSlice. When called with an index, it returns an action object that describes
+      the change we want to make to the state (in this case, decrementing the quantity of a specific venue item).
+      
+      When we call dispatch(decrementQuantity(index)), we're sending that action to the Redux store.
+
+      The store will then use the reducer function defined in venueSlice to determine how to update the state based on the action received.
+      */}
+    
+
+
   const handleRemoveFromCart = (index) => {
     if (venueItems[index].quantity > 0) {
       dispatch(decrementQuantity(index));
     }
   };
   const handleIncrementAvQuantity = (index) => {
+        dispatch(incrementAvQuantity(index));
   };
 
-  const handleDecrementAvQuantity = (index) => {
+  const handleDecrementAvQuantity = (index) => {  
+        dispatch(decrementAvQuantity(index));
   };
 
   const handleMealSelection = (index) => {
@@ -45,17 +72,23 @@ const ConferenceEvent = () => {
   const items = getItemsFromTotalCost();
 
   const ItemsDisplay = ({ items }) => {
-
   };
-  const calculateTotalCost = (section) => {
+ 
+ const calculateTotalCost = (section) => {
     let totalCost = 0;
     if (section === "venue") {
       venueItems.forEach((item) => {
         totalCost += item.cost * item.quantity;
       });
+    } else if (section === "av") {
+      avItems.forEach((item) => {
+        totalCost += item.cost * item.quantity;
+      });
     }
     return totalCost;
   };
+
+  const avTotalCost = calculateTotalCost("av");
   const venueTotalCost = calculateTotalCost("venue");
 
   const navigateToProducts = (idType) => {
@@ -157,9 +190,29 @@ const ConferenceEvent = () => {
 
                 </div>
                 <div className="addons_selection">
+                  {/* Below is a common React pattern. .map() iterates through the avItems array, each time calling
+                      the function, which returns JSX for React to render.
+                      item is the current array element and index its position in the array.
+                      I'm using the index as the key for each element, but that is only recommended for static lists.
+                      The course recommends I use a unique identifier as part of the element (e.g., item.id) when
+                      list order can change. In Rect terms, React likes the items it renders to have a stable key prop.*/}
+                  {avItems.map((item, index) => (
+                    <div className="av_data venue_main" key={index}>
+                      <div className="img">
+                        <img src={item.img} alt={item.name} />
+                      </div>
+                      <div className="text"> {item.name} </div>
+                      <div> ${item.cost} </div>
+                      <div className="addons_btn">
+                        <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                        <span className="quantity-value">{item.quantity}</span>
+                        <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                      </div>
+                    </div>
+                  ))}
 
                 </div>
-                <div className="total_cost">Total Cost:</div>
+                <div className="total_cost">Total Cost: {avTotalCost}</div>
 
               </div>
 
@@ -185,7 +238,8 @@ const ConferenceEvent = () => {
             </div>
           ) : (
             <div className="total_amount_detail">
-              <TotalCost totalCosts={totalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
+{/* TODO Commented this next line out as it's not working yet.              
+              <TotalCost totalCosts={totalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} /> */}
             </div>
           )
         }
--- src/avSlice.js
+++ src/avSlice.js
@@ -3,19 +3,55 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      img: "https://humanlegion.com/wp-content/uploads/2023/08/small-chimeracompany-6-THELASTREDOUBT-cover.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "https://humanlegion.com/wp-content/uploads/2023/03/TimCTaylor_The_Good_Soldier_vejk_as_a_space_soldier_mchimp.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "https://humanlegion.com/wp-content/uploads/2023/03/TimCTaylor_the_last_redoubt_mchimp.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "https://humanlegion.com/wp-content/uploads/2023/09/chimera-dept9-vertical_400px.jpg",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+
+    {
+      img: "https://humanlegion.com/wp-content/uploads/2024/03/TD-3-1984-SQUARE-800px.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
+
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+        item.quantity--;
+      }
     },
-  },
-});
+  }});
 
 export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
 
